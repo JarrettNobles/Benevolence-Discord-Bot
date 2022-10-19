ba9d40a94f017a05b21b0ba3ff4c7b8348bb27fb
Jarrett Nobles <49537875+jarebear6136@users.noreply.github.com>
2022-10-18 21:59:44 -0400

    Add files via upload

---
+++ DiscordBot/src/main/java/org/example/darkwaterBot.java
@@ -0,0 +1,39 @@
+package org.example;
+//made by Jarrett Nobles
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
+public class darkwaterBot {
+
+    private final ShardManager shardManager;
+
+    //constructor
+    public darkwaterBot()throws LoginException {
+        String token = "MTAzMjEwMzQ5MzA0Mjk3ODgzNg.GeflIa.lqmQksT9bkLw8OOXUoKosgyTm4BhSXDbCQK9aA";
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.watching("for media in general"));
+        builder.build();
+        shardManager = builder.build();
+
+    }
+
+    public ShardManager getShardManager(){
+        return shardManager;
+    }
+
+    //main method
+    public static void main(String[] args){
+        try {
+            darkwaterBot bot = new darkwaterBot();
+        }
+        catch(LoginException e){
+            System.out.println("Error: BOT TOKEN IS INVALID");
+        }
+
+    }
+}
