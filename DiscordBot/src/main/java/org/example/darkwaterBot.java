e3be734a4257b181a06d002600fa00daa666018e
Jarrett Nobles <noblesjarrett035@gmail.com>
2022-10-18 22:49:55 -0400

    Changed where it does not add the bot token in source code but instead uses env example. (see .env.example).

--- DiscordBot/src/main/java/org/example/darkwaterBot.java
+++ DiscordBot/src/main/java/org/example/darkwaterBot.java
@@ -1,5 +1,6 @@
 package org.example;
 //made by Jarrett Nobles
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
@@ -8,12 +9,15 @@ import net.dv8tion.jda.api.sharding.ShardManager;
 import javax.security.auth.login.LoginException;
 
 public class darkwaterBot {
-
+    private final Dotenv config;
     private final ShardManager shardManager;
 
     //constructor
     public darkwaterBot()throws LoginException {
-        String token = "TEST_TOKEN";
+        //load .env file
+        config = Dotenv.configure().load();
+        //get the config for the bot token
+        String token=config.get("TOKEN");
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.watching("for media in general"));
@@ -21,6 +25,10 @@ public class darkwaterBot {
         shardManager = builder.build();
         //test commit
 
+    }
+
+    public Dotenv getConfig() {
+        return config;
     }
 
     public ShardManager getShardManager(){
